d59ab8ce4edb9f8007ae5713ddf53d17bab9488a
Frank Hunleth <fhunleth@troodon-software.com>
2023-01-03 22:00:11 -0500

    nerves_system_br: bump to v1.22.1

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule NervesSystemMangopiMQPro.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.6.0 or ~> 1.7.15 or ~> 1.8", runtime: false},
-      {:nerves_system_br, "1.22.0", runtime: false},
+      {:nerves_system_br, "1.22.1", runtime: false},
       {:nerves_toolchain_riscv64_nerves_linux_gnu, "~> 1.7.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false}
